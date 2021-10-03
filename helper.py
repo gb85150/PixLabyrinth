53e7afdacf232177ae26a7766ec50dad0ef1f574
gb85150 <geoffrey.bousseau@stfrancoislaroche.fr>
2021-10-03 11:59:52 +0200

    definition de generate() pour publication

--- helper.py
+++ helper.py
@@ -1,11 +1,18 @@
 import random as r
 
 
-def generate(choice: list, weight: list):
+def generate(choice: list, weight: list) -> str:
+    """
+    :version: 1.0
+    Fonction permettant un tirage au sort (random) selon des probabilités fixées sur 100
+    :param choice: list of choices
+    :param weight: weight of these choices
+    :return: str (the choice selected)
+    """
+
     for i in range(len(choice)):
         coefficient = [k for k in range(weight[i])]
     result = r.randint(0, 100)
     for i in range(len(choice)):
         if result in coefficient[i]:
             return choice[i]
-    pass
